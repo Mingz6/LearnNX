0db7478a1c3c04f562126ac8856e4d3047c8d612
Ming <mingz6@outlook.com>
2023-02-27 14:49:28 -0700

    Import storybook from main storybook

--- react/libs/shared-ui/.storybook/main.ts
+++ react/libs/shared-ui/.storybook/main.ts
@@ -1,15 +1,16 @@
-import type { StorybookConfig } from '@storybook/core-common';
-
+import { rootMain } from '../../../.storybook/main';
+import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   core: { builder: '@storybook/builder-vite' },
   stories: [
+    ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-essentials'],
+  addons: [...rootMain.stories],
   async viteFinal(config: any) {
     return mergeConfig(config, {
       plugins: [
