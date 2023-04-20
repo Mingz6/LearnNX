5ba716695ebab9289a257747cf70145a1f329412
Ming <mingz6@outlook.com>
2023-04-20 15:49:41 -0600

    test yml

--- react/apps/mingzsite/src/app/myapp.tsx
+++ react/apps/mingzsite/src/app/myapp.tsx
@@ -3,8 +3,7 @@ import { SharedUi } from '@learnnx/shared-ui';
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  // test changes to shared-ui
-  
+
   return (
     <>
       <NxWelcome title="mingzsite" />
