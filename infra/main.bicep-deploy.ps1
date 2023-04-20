e0c4089443e17be30ec9d1f6bcb9a14d3356b575
Ming <mingz6@outlook.com>
2023-04-20 14:54:03 -0600

    get proper name

--- infra/main.bicep-deploy.ps1
+++ infra/main.bicep-deploy.ps1
@@ -3,7 +3,7 @@
 # Target Resource Group name, FIlename for template parameters
 param ($resourceGroup, $parametersFileName) 
 
-az deployment group create --resource-group $resourceGroup --subscription "CRNA - CSP" --template-file ./main.bicep --parameters $parametersFileName --name ($resourceGroup + (Get-Date -Format "MMM-dd-yyyy.hh.mm.tt"))
+az deployment group create --resource-group $resourceGroup --subscription "CRNA - CSP" --template-file ./main.bicep --parameters $parametersFileName --name ($resourceGroup + "-" + (Get-Date -Format "MMM-dd-yyyy.hh.mm.tt"))
 
 # Delete a resource if fail. E.g. Static Web App
 # az resource delete --resource-group development-ming --name stapp-learnnx-dev --resource-type "Microsoft.Web/staticSites" --verbose
