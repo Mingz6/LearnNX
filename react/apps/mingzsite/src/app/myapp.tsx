ce0825375b062488e932f7498e51517d748fd8b5
Ming <mingz6@outlook.com>
2023-04-20 15:32:25 -0600

    init test web app

---
+++ react/apps/mingzsite/src/app/myapp.tsx
@@ -0,0 +1,17 @@
+import { SharedUi } from '@learnnx/shared-ui';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="mingzsite" />
+
+      <SharedUi />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
